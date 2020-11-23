e5cde896c81a4cb1c6cf8bbdfa9d2811bed7df93
oneofthedevs <iamdevchhaniyara@gmail.com>
2020-11-23 16:15:11 +0530

    :sparkles: Moved NewLinkForm in spearate component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="home">
     <div class="main-section">
-      <div class="form">
-      <input class="input" type="text" name="url" v-model="originalUrl" placeholder="e.g. http://www.google.com" autocomplete="off">
-      <button class="btn btn-default" @click="submit">Click to shorten</button>
-      </div>
+     <LinkForm @generate-link="submit" />
     </div>
     <div class="generated" v-if="response">
       <span>Generated Link: </span><a class="link" :href="response.response.shortUrl" target="_blank">{{response.response.shortUrl}}</a>
@@ -13,8 +10,12 @@
 </template>
 
 <script>
+import LinkForm from '../components/NewLinkForm';
 export default {
   name: 'Home',
+  components: {
+    LinkForm
+  },
   data: () => {
     return {
       originalUrl: '',
@@ -22,23 +23,22 @@ export default {
     }
   },
   methods: {
-    async submit() {
+    async submit(url) {
       const requestOptions = {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ url: this.originalUrl })
+        body: JSON.stringify({ url: url })
       };
       const res = await fetch('http://localhost:5000/generateURL', requestOptions);
-      const data = await res.json()
-      
-      this.response = data;
-      console.log(this.response.response.shortUrl);
+      this.response = await res.json()
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+@import '../assets/styles/style.scss';
+
 .home{ 
   min-height: 100vh;
   display: flex;
@@ -48,27 +48,15 @@ export default {
 }
 
 .main-section {
+  width: 100%;
   display: flex;
+  justify-content: center;
 }
 .form {
-  flex-basis: 1000px;
-}
-
-.input[type=text] {
-  flex: 1;
-  padding: 10px 20px;
-  background: #eee;
-  border: 0;
-  border-radius: 5px;
-  border: 2px solid transparent;
-  margin: {
-    right: 5px
-  };
-}
-
-.input[type=text]:focus {
-  border: 2px solid #FFAC53;
-  // outline: 1px solid #FFAC53;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  width: 90%;
 }
 
 .generated{
@@ -80,24 +68,11 @@ export default {
   display: inline;
   text-decoration: none;
   font-size: 17px;
-  color: #3e3e3e;
-  background: #FFAC53;
+  color: $dark;
+  background: $orange;
   padding: 10px 20px;
   color: #fff;
   border-radius: 5px;
-}
-
-.btn {
-  padding: 10px 20px;
-  border: 0;
-  cursor: pointer;
-  border-radius: 5px;
-}
-
-.btn-default {
-  border: 2px solid #FFAC53;
-  background: #FFAC53;
-  color: #fff;
 }
 
 </style>
