930ea42244323030336aa3f67f58b9369af72d59
oneofthedevs <iamdevchhaniyara@gmail.com>
2020-11-23 18:16:34 +0530

    :art: Added navbar and made changes in form component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,62 @@
+<template>
+  <nav>
+    <div class="container-div">
+      <div class="titled-div">
+        <h1 class="title">lazy URL</h1>
+      </div>
+      <div class="navigation-div">
+        <ul class="nav-links">
+          <li class="nav-item"><router-link :to="{name: 'Home'}" class="nav-link">Home</router-link></li>
+          <li class="nav-item"><router-link :to="{name: 'Analytics'}" class="nav-link">Analytics</router-link></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style  lang="scss" scoped>
+@import '../assets/styles/style.scss';
+
+nav{
+  width: 100%;
+  background: $orange;
+}
+title {
+  margin: 10px 0;
+}
+.container-div {
+  margin: 0 5%;
+  display: flex;
+}
+.navigation-div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.titled-div {
+  display: flex;
+  align-items: center;
+  flex: 1;
+}
+.nav-links {
+  display: flex;
+  flex-wrap: nowrap;
+}
+.nav-item {
+  display: flex;
+}
+.nav-link {
+  text-decoration: none;
+  color: #fff;
+  padding: 10px 15px;
+  border-bottom: 4px solid transparent;
+
+}
+.nav-link:hover {
+  border-bottom: 4px solid darkorange;
+}
+</style>
--- src/components/NewLinkForm.vue
+++ src/components/NewLinkForm.vue
@@ -1,43 +1,33 @@
 <template>
-    <div class="form">
-      <input class="input" type="text" name="url" v-model="originalUrl" placeholder="e.g. https://www.google.com" autocomplete="off">
-      <button class="btn btn-default" @click="onSubmit">Click to shorten</button>
-    </div>
+  <div class="form">
+    <input
+      class="input"
+      type="text"
+      name="url"
+      v-model="originalUrl"
+      placeholder="e.g. https://www.google.com"
+      autocomplete="off"
+    />
+    <button
+      class="btn btn-default"
+      @click="this.$emit('generate-link', this.originalUrl)"
+    >
+      Click to shorten
+    </button>
+  </div>
 </template>
 
 <script>
 export default {
-name: 'LinkForm',
-data: () => {
-  return {
-    originalUrl: ''
-  }
-},
-methods: {
-  onSubmit() {
-    this.$emit('generate-link', this.originalUrl);
-  }
-}
-}
+  name: "LinkForm",
+  data: () => {
+    return {
+      originalUrl: "",
+    };
+  },
+};
 </script>
 
 <style lang="scss">
-@import '../assets/styles/style.scss';
-
-.input[type=text] {
-  flex: 1;
-  padding: 10px 20px;
-  background: #eee;
-  border: 0;
-  border-radius: 5px;
-  border: 2px solid transparent;
-  margin: {
-    right: 5px
-  };
-}
-
-.input[type=text]:focus {
-  border: 2px solid #FFAC53;
-  /* // outline: 1px solid #FFAC53; */
-}
+@import "../assets/styles/style.scss";
 </style>
