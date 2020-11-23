3f4af7ac38df10e7d17731c237bed94b60f5624b
oneofthedevs <iamdevchhaniyara@gmail.com>
2020-11-23 18:18:42 +0530

    :sparkles: Added Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
+  <Navbar />
   <!-- <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div> -->
   <router-view/>
 </template>
-
+<script>
+import Navbar from './components/Navbar';
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
